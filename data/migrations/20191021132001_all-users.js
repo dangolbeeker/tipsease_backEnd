bec7a51c58c9d5f7b82eb6c7521da06b11efd779
MadisonViaLux <madison.s.mckown@gmail.com>
2019-10-21 14:20:52 -0700

    10/21/19 morning settup

---
+++ data/migrations/20191021132001_all-users.js
@@ -0,0 +1,50 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('customers', tbl => {
+        tbl.increments()
+
+        tbl.string('username', 120)
+            .notNullable()
+            .unique();
+        tbl.string('password', 120)
+            .notNullable()
+        tbl.string('FirstName', 120)
+            .notNullable()
+        tbl.string('LastName', 120)
+            .notNullable()
+        tbl.string('email', 120)
+            .notNullable()
+        tbl.boolean('customerOrService')
+    })
+    .createTable('serviceWorker', tbl => {
+        tbl.increments()
+
+        tbl.string('username', 120)
+            .notNullable()
+            .unique();
+        tbl.string('password', 120)
+            .notNullable()
+        tbl.string('FirstName', 120)
+            .notNullable()
+        tbl.string('LastName', 120)
+            .notNullable()
+        tbl.string('email', 120)
+            .notNullable()
+        tbl.boolean('customerOrService')
+        tbl.string('tagline', 80)
+            .notNullable()
+        tbl.string('company')
+            .notNullable()
+        tbl.integer('YearsAtCompany')
+            .notNullable()
+        tbl.integer('balance')
+            .notNullable()
+
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('customers')
+        .dropTableIfExists('serviceWorker')
+};
---
+++ index.js
@@ -0,0 +1,4 @@
+const server = require('./api/server')
+
+const port = process.env.PORT || 7777
+server.listen(port, () => console.log(`\n\trunning on http://localhost:${port}\n`))
