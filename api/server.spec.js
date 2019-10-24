f0a9fe1751b77200e727168bc49489383a3d76ff
MadisonViaLux <madison.s.mckown@gmail.com>
2019-10-24 16:00:35 -0700

    10/24/19 adding testing

---
+++ api/server.spec.js
@@ -0,0 +1,24 @@
+// const request = require('supertest')
+// const server = require('./server')
+
+
+
+// it('should set testing env', () => {
+//     expect(process.env.DB_ENV).toBe('testing')
+// });
+
+// it('server using CORS', () => {
+//     expect(server.use).toBe(cors())
+// })
+
+// describe('GET /', () => {
+
+//     it('return status 200', () => {
+//         return request(server)
+//             .get('/')
+//             .then(res => {
+//                 expect(res.status).toBe(200)
+//             })
+//     })
+
+// })
--- data/db-config.js
+++ data/db-config.js
@@ -4,3 +4,10 @@ const config = require('../knexfile.js');
 const env = process.env.DB_ENV || 'development';
 
 module.exports = knex(config[env]);
+
+
+// const knex = require('knex');
+
+// const knexConfig = require('../knexfile.js');
+
+// module.exports = knex(knexConfig.development);
