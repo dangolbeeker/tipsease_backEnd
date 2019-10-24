2a02bb01369e311ec61e5c3a6f8dde109312cc59
MadisonViaLux <madison.s.mckown@gmail.com>
2019-10-24 10:13:21 -0700

    10/24/19 routes update v3

--- users/customers/customer-router.js
+++ users/customers/customer-router.js
@@ -56,7 +56,7 @@ router.post('/login', (req, res) => {
         .then(user => {
             if(user && bcrypt.compareSync(password, user.password)){
                 const token = customerToken(user)
-                res.status(200).json({note: `user ${user.username} has a token`, token})
+                res.status(200).json(user, token)
             } else {
                 res.status(401).json({ message: 'Invalid creds my dude' });
             }
