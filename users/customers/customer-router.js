84063f895e56310a89def8e701d9fe7c3cf108b8
MadisonViaLux <madison.s.mckown@gmail.com>
2019-10-24 11:33:43 -0700

    10/24/19 strange route fixes

--- users/customers/customer-router.js
+++ users/customers/customer-router.js
@@ -48,21 +48,42 @@ router.post('/signup', (req, res) => {
 })
 
 
-router.post('/login', (req, res) => {
-    const { username, password } = req.body
 
-    db.findBy({username})
-        .first()
-        .then(user => {
-            if(user && bcrypt.compareSync(password, user.password)){
-                const token = customerToken(user)
-                res.status(200).json({note: `user ${user.username} has a token`, token})
-            } else {
-                res.status(401).json({ message: 'Invalid creds my dude' });
-            }
-        })
-        .catch(err => res.status(500).json({err: 'Missing creds'}))
-})
+router.post('/login', (req, res) => {
+    const {username, password} = req.body
+  
+    db.findBy({ username })
+      .first()
+      .then(person => {
+        if(person && bcrypt.compareSync(password, person.password)){
+          const token = customerToken(person)
+          res.status(200).json({person, token})
+        } else {
+          res.status(401).json({message: 'Invalid creds my dude'})
+        }
+      })
+      .catch(err => res.status(500).json({message: 'Missing creds'}))
+  });
+
+
+
+
+
+// router.post('/login', (req, res) => {
+//     const { username, password } = req.body
+
+//     db.findBy({username})
+//         // .first()
+//         .then(user => {
+//             if(user && bcrypt.compareSync(password, user.password)){
+//                 const token = customerToken(user)
+//                 res.status(200).json({user, token})
+//             } else {
+//                 res.status(401).json({ message: 'Invalid creds my dude' });
+//             }
+//         })
+//         .catch(err => res.status(500).json({err: 'Missing creds'}))
+// })
 
 // blocked,
 router.delete('/:id',  (req, res) => {
